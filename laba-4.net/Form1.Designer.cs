0ef2e3b5553648c27d704da034d9f7c214f11d3e
ANDREW-NB\numva <numv.andrew@gmail.com>
2021-04-27 13:43:15 +0300

    Итогоовый вариант. Проект написан на framework4.7.2

---
+++ laba-4.net/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace laba_4.net
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button3.Location = new System.Drawing.Point(302, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "ЗАПИСАТЬ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button2.Location = new System.Drawing.Point(178, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "СЧИТАТЬ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (254)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "ПОСТРОИТЬ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        #endregion
+    }
+}
---
+++ laba-4.net/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace laba_4.net
+{
+    public partial class Form1 : Form
+    {
+        private List<Graph> Mass = new List<Graph>();
+        private int maxy, count;
+
+        private class Graph
+        {
+            internal int Higth { get; set; }
+            internal Color Color { get; }
+
+            public Graph(int higth, int R, int G, int B)
+            {
+                Higth = higth;
+                Color = Color.FromArgb(R, G, B);
+            }
+        }
+       
+        public Form1()
+        {
+            InitializeComponent();
+            Create("Data1.txt");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            count = r.Next(5, 20);
+            Mass.Clear();
+            maxy = 0;
+            
+            for (int i = 0; i < count; i++)
+            {
+                Graph temp = new Graph(r.Next(-1000, 1000),r.Next(255),r.Next(255),r.Next(255));
+                Mass.Add(temp);
+                if (Math.Abs(temp.Higth) > maxy) maxy = Math.Abs(temp.Higth);
+            }
+
+            Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Read("Data1.txt");
+            Invalidate();
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Write("Data2.txt");
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            //рисуем поле 
+            int xc = 100;
+            int yc = (this.Height) / 2;
+            g.TranslateTransform(xc, yc);
+            g.DrawLine(new Pen(Color.Black, 2f), 0, -250, 0, 250);
+            g.DrawLine(new Pen(Color.Black, 2f), 0, 0, 1000, 0);
+            
+            float step = 1000f / (float)Mass.Count;
+            float k = Math.Abs(250f / (float)maxy);
+            
+            Random rn = new Random();
+            
+            SolidBrush br = new SolidBrush(Color.Black);
+            Font stringFont = new Font("Times New Roman", 10, FontStyle.Regular);
+            SizeF ss = new SizeF();
+
+            for (var i = 0; i < Mass.Count; i++)
+            {
+                float width = step / 2;
+                br.Color = Mass[i].Color;
+                ss = e.Graphics.MeasureString((Mass[i].Higth).ToString(), stringFont);
+                if (Mass[i].Higth < 0)
+                {
+                    g.FillRectangle(br,(i + 0.5f)*step,1f,width,-1f*Mass[i].Higth*k);
+                    g.DrawString((Mass[i].Higth).ToString(), stringFont, new SolidBrush(Color.Black), new PointF((i + 0.75f)*step-ss.Width/2, -(Mass[i].Higth*k)));
+                }
+                if (Mass[i].Higth >= 0)
+                {
+                    g.FillRectangle(br,(i + 0.5f)*step,((-1)*Mass[i].Higth*k)-1,width,Mass[i].Higth*k);
+                    g.DrawString((Mass[i].Higth).ToString(), stringFont, new SolidBrush(Color.Black), new PointF((i + 0.75f)*step-ss.Width/2, -(Mass[i].Higth*k+15)));
+                }
+            }
+
+            
+        }
+
+        void Create(string path)
+        {
+            Random r = new Random();
+            count = r.Next(5, 20);
+            StreamWriter cr = new StreamWriter(path);
+            
+            var sb = new StringBuilder();
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append((r.Next(-1000, 1000)).ToString());
+                sb.Append(":");
+                for (var k = 0; k < 3; k++)
+                {
+                    sb.Append((r.Next(255)).ToString());
+                    if (k != 2)sb.Append(":") ;
+                }
+                cr.Write(sb+ "\n");
+                sb.Clear();
+            }
+
+            cr.Close();
+        }
+
+        void Write(string path)
+        {
+            StreamWriter cr = new StreamWriter(path);
+            
+            var sb = new StringBuilder();
+            for (int i = 0; i < Mass.Count; i++)
+            {
+                sb.Append((Mass[i].Higth).ToString());
+                sb.Append(":");
+                sb.Append(Mass[i].Color.R.ToString());
+                sb.Append(":") ;
+                sb.Append(Mass[i].Color.G.ToString());
+                sb.Append(":") ;
+                sb.Append(Mass[i].Color.B.ToString());
+                
+                cr.Write(sb+ "\n");
+                sb.Clear();
+            }
+
+            cr.Close();
+        }
+        
+        void Read(string path)
+        {
+            StreamReader file = new StreamReader(path, Encoding.Default);
+            string line;
+            Mass.Clear();
+            maxy = 0;
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] numb = line.Split(':');
+                Graph temp = new Graph(int.Parse(numb[0]),int.Parse(numb[1]),int.Parse(numb[2]),int.Parse(numb[3]));
+                Mass.Add(temp);
+                if (Math.Abs(temp.Higth) > maxy) maxy = Math.Abs(temp.Higth);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
